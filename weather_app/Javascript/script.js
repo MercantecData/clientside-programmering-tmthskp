c3aa9e4cd0e4c454c394f7bab9156cf3fc2cbd89
Tommy <tmth.skp@edu.mercantec.dk>
2020-09-03 13:30:00 +0200

    Har fået dataen ud og kan manipuler med by navne nu via en formular
    målet i morgen af at gøre visningen af data pænere samt lege med muligheder i kaldet

--- weather_app/Javascript/script.js
+++ weather_app/Javascript/script.js
@@ -1,8 +1,23 @@
 //OpenWeather Script city name Viborg
+
+//Dette er en statisk måde at få data ud på site. Vil prøve at få det til at virke med formular
+//var url ="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}"
+var url = "https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=8d25bcbc04528882c5fbe036421e9aeb"
+
+fetch(url)
+.then(data=>data.json())
+.then(SendDatatoPage)
+
+function SendDatatoPage(data)
+{
+    document.getElementById("land").innerHTML = `Land ${data.sys.country}`
+    document.getElementById("by_navn").innerHTML = `Vejret i ${data.name}`
+    document.getElementById("temperatur").innerHTML = `Temperatur ${Math.floor(data.main.temp) - 273,15}° grader`//Math.floor() fjerner komma tal
+}
+
+//Forsøg med by navn Viborg
 function openWeatherMapScript()
 {
-
-  //var url ="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}"
   var url = "https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=8d25bcbc04528882c5fbe036421e9aeb"
 
   var promise = fetch(url)
@@ -13,6 +28,7 @@ function openWeatherMapScript()
 //Openweather Script kald city name Viborg
 openWeatherMapScript();
 
+//Forsøg  med by navn thisted
 function openWeatherMapScriptThisted()
 {
   var url = "https://api.openweathermap.org/data/2.5/weather?q=Thisted&appid=8d25bcbc04528882c5fbe036421e9aeb"
@@ -26,6 +42,7 @@ function openWeatherMapScriptThisted()
 function openWeatherMapCords()
 {
     var url = "https://api.openweathermap.org/data/2.5/weather?lat=56.45&lon=9.4&appid=8d25bcbc04528882c5fbe036421e9aeb"
+    
     var promise = fetch(url)
     var promise2 = promise.then(data=>data.text())
     promise2.then(data=>{document.getElementById("cord_fetch").innerHTML=data;})
@@ -35,44 +52,38 @@ function openWeatherMapCords()
 function openWeatherMapOneCall()
 {
     var url = "https://api.openweathermap.org/data/2.5/onecall?lat=56.45&lon=9.4&exclude=daily,minutely,hourly&appid=8d25bcbc04528882c5fbe036421e9aeb"
+    
     var promise = fetch(url)
     var promise2 = promise.then(data=>data.text())
     promise2.then(data=>{document.getElementById("cord_fetch_one_call").innerHTML=data;})
 }
 
-
-//Forsøg med formular input
+//Forsøg med formular input specifik data
 function openWeatherMapScriptCityNameInput()
 {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q="+document.getElementById("city_name").value+"&appid=8d25bcbc04528882c5fbe036421e9aeb"
-
-    var promise = fetch(url)
-    var promise2 = promise.then(data=>data.text())
-    promise2.then(data=>document.getElementById("city_name_fetch_input").innerHTML=data)
+    var url_1 = "https://api.openweathermap.org/data/2.5/weather?q="+document.getElementById("city_name").value+"&appid=8d25bcbc04528882c5fbe036421e9aeb"
     
+    fetch(url_1)
+    .then(data=>data.json())
+    .then(SendDatatoPage_1)   
 }
 
-//Forsøg med formular input vis udvalgte data
-function openWeatherMapScriptCityNameInputChoose()
+function SendDatatoPage_1(data)
+{
+    document.getElementById("land_1").innerHTML = `Land ${data.sys.country}`
+    document.getElementById("by_navn_1").innerHTML = `Vejret i ${data.name}`
+    document.getElementById("temperatur_1").innerHTML = `Temperatur ${Math.floor(data.main.temp) - 273,15}° grader i Kelvin`
+}
+
+//Forsøg med formular input alt data
+function openWeatherMapScriptCityNameInput_1()
 {
     var url = "https://api.openweathermap.org/data/2.5/weather?q="+document.getElementById("city_name_1").value+"&appid=8d25bcbc04528882c5fbe036421e9aeb"
-
+   
     var promise = fetch(url)
     var promise2 = promise.then(data=>data.text())
-    promise2.then(data=>document.getElementById("city_name_fetch_input_1").innerHTML = data.main.temp)
+    promise2.then(data=>document.getElementById("city_name_fetch_input").innerHTML=data)  
+}
+
+
     
-}
-
-//Dette er en statisk måde at få data ud på site. Vil prøve at få det til at virke med formular
-var url = "https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=8d25bcbc04528882c5fbe036421e9aeb"
-var result
-
-fetch(url)
-.then(data=>data.json())
-.then(UpdatetoPage)
-
-function UpdatetoPage(data)
-{
-    document.getElementById("content").innerHTML = `Vejret i ${data.name}`
-    document.getElementById("temperatur").innerHTML = `${data.main.temp} grader`
-}
